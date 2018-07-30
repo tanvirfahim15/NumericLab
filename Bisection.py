ee838e744898852b3b3e20e20a21aaca4791e088
tanvir <tanvircsedu15@gmail.com>
2018-07-31 01:18:46 +0600

    Lab 01 Done

--- Bisection.py
+++ Bisection.py
@@ -1,7 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math
-
 class state:
     def __init__(self, x_n, x_p, x_m, err, f_xm):
         self.x_n = x_n
@@ -10,10 +8,8 @@ class state:
         self.err = err
         self.f_xm = f_xm
 
-
 class Bisection:
-
-    def __init__(self, x1, x2):
+     def __init__(self, x1, x2):
         if Bisection.function(x1)<0 < Bisection.function(x2):
             self.x_neg = x1
             self.x_pos = x2
@@ -21,30 +17,20 @@ class Bisection:
             self.x_neg = x2
             self.x_pos = x1
         else:
-            self.x_neg = x2
-            self.x_pos = x1
-            '''raise ValueError('Solution Impossible')'''
+            raise ValueError('Invalid Initial Value')
         self.x_mid = None
 
-    @staticmethod
-    def function(x):
-        n=0
-        ans = 1
-        temp = 0.0
-        for k in range(1, 11):
-            a = pow(-1, k)
-            b = pow((pow(x, 2) / 4), k)
-            c = math.factorial(k) * math.factorial(k)
-            temp += a * b / c;
-        return ans * temp
+     @staticmethod
+     def function(x):
+        return pow(x, 3)-0.165*pow(x, 2)+3.993*pow(10, -4)
 
-    @staticmethod
-    def error(x_new, x_old):
+     @staticmethod
+     def error(x_new, x_old):
         if x_old is None:
             return None
-        return math.fabs((x_new-x_old)/x_new)
+        return 100*(x_new-x_old)/x_new
 
-    def run(self):
+     def run(self):
         new_mid = (self.x_neg + self.x_pos) / 2
         error = Bisection.error(new_mid, self.x_mid)
         self.x_mid = new_mid
@@ -57,61 +43,24 @@ class Bisection:
         return st
 
 
-x=1
-while x<3.1:
-    print(x)
-    print(Bisection.function(x))
-    print()
-    x+=0.1
-
-tolerance_flag = False
-if input()=='1':
-    tolerance_flag = True
-
 bs = Bisection(float(input('Enter x1: ')), float(input('Enter x2: ')))
-
+iterations = int(input('Enter Iterations: '))
 xm, err, fxm = [], [], []
-
-if tolerance_flag is False:
-    iterations = int(input('Enter Iterations: '))
-    for i in range(iterations):
-        st = bs.run()
-        print(i + 1, st.x_n, st.x_p, st.x_m, st.err, st.f_xm)
-        xm.append(st.x_m)
-        err.append(st.err)
-        fxm.append(st.f_xm)
-else:
-    tolerance = pow(10, -int(input('Enter Tolerance:')))
-    temp_tolerance = None
-    i = 0
-    while temp_tolerance is None or temp_tolerance>tolerance:
-        st = bs.run()
-        print(i + 1, st.x_n, st.x_p, st.x_m, st.err, st.f_xm)
-        xm.append(st.x_m)
-        err.append(st.err)
-        fxm.append(st.f_xm)
-        temp_tolerance=st.err
-        i += 1
-
-
-'''
+for i in range(iterations):
+    st = bs.run()
+    print(i+1, st.x_n, st.x_p, st.x_m, st.err, st.f_xm)
+    xm.append(st.x_m)
+    err.append(st.err)
+    fxm.append(st.f_xm)
 plt.plot(xm)
 plt.ylabel('X mid')
 plt.xlabel('Iterations')
 plt.show()
-'''
 plt.ylabel('Error')
-plt.xlabel('xm')
-plt.plot(xm,err)
-plt.show()
-plt.clf()
-plt.ylabel('it')
-plt.xlabel('err')
-'''
-ax1 = plt.subplot()
-
-ax1.set_ylim(-.0005,.0005)
-ax1.set_xlim(0.05,.09)
-'''
+plt.xlabel('Iterations')
 plt.plot(err)
+plt.show()
+plt.ylabel('F(X mid)')
+plt.xlabel('Iterations')
+plt.plot(fxm)
 plt.show()
--- FalsePositionMethod.py
+++ FalsePositionMethod.py
@@ -1,6 +1,6 @@
-
+import numpy as np
 import matplotlib.pyplot as plt
-import math
+
 
 class state:
     def __init__(self, x_n, x_p, x_m, err, f_xm):
@@ -12,8 +12,7 @@ class state:
 
 
 class FalsePosition:
-
-    def __init__(self, x1, x2):
+     def __init__(self, x1, x2):
         if FalsePosition.function(x1)<0 < FalsePosition.function(x2):
             self.x_neg = x1
             self.x_pos = x2
@@ -22,29 +21,25 @@ class FalsePosition:
             self.x_pos = x1
         else:
             raise ValueError('Invalid Initial Value')
-
         self.x_mid = None
 
-    @staticmethod
-    def function(x):
-        ca0 = 42
-        cb0 = 28
-        cc0 = 4
-        k = 0.016
-        return ((cc0 + x) / (pow((ca0 - 2 * x), 2) * (cb0 - x))) - k
 
-    @staticmethod
-    def error(x_new, x_old):
+     @staticmethod
+     def function(x):
+        return pow(x-4, 2)*(x+2)\
+
+     @staticmethod
+     def error(x_new, x_old):
         if x_old is None:
             return None
-        return math.fabs((x_new-x_old)/x_new)
+        return 100*(x_new-x_old)/x_new
 
-    @staticmethod
-    def get_mid(x1,x2):
+     @staticmethod
+     def get_mid(x1,x2):
         return ((-FalsePosition.function(x1)*(x1-x2))/(FalsePosition.function(x1)-FalsePosition.function(x2)))+x1
 
-    def run(self):
-        new_mid = FalsePosition.get_mid(self.x_neg,self.x_pos)
+     def run(self):
+        new_mid = FalsePosition.get_mid(self.x_pos,self.x_neg)
         error = FalsePosition.error(new_mid, self.x_mid)
         self.x_mid = new_mid
         f_x = FalsePosition.function(self.x_mid)
@@ -57,26 +52,24 @@ class FalsePosition:
 
 
 fp = FalsePosition(float(input('Enter x1: ')), float(input('Enter x2: ')))
-
+iterations = int(input('Enter Iterations: '))
 xm, err, fxm = [], [], []
 
-
-tolerance = pow(10, -int(input('Enter Tolerance:')))
-temp_tolerance = None
-i = 0
-while temp_tolerance is None or temp_tolerance > tolerance:
+for i in range(iterations):
     st = fp.run()
-    print(i + 1, st.x_n, st.x_p, st.x_m, st.err, st.f_xm)
+    print(i+1, st.x_n, st.x_p, st.x_m, st.err, st.f_xm)
     xm.append(st.x_m)
     err.append(st.err)
     fxm.append(st.f_xm)
-    temp_tolerance = st.err
-    i += 1
-
-plt.ylabel('Error')
-plt.xlabel('xm')
-plt.plot(xm,err)
+plt.plot(xm)
+plt.ylabel('X mid')
+plt.xlabel('Iterations')
 plt.show()
+plt.ylabel('Error')
+plt.xlabel('Iterations')
 plt.plot(err)
 plt.show()
-
+plt.ylabel('F(X mid)')
+plt.xlabel('X mid')
+plt.scatter(xm,fxm)
+plt.show()
