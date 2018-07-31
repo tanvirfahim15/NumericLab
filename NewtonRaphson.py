037fa92bf1b2719f666a19397d6eaef9fea412cb
tanvir <tanvircsedu15@gmail.com>
2018-08-01 01:56:46 +0600

    Lab 01 Done

---
+++ NewtonRaphson.py
@@ -0,0 +1,81 @@
+import math
+import matplotlib.pyplot as plt
+
+
+class Status:
+    def __init__(self,i, x, fx, dfx, err):
+        self.i = i
+        self.x = x
+        self.fx = fx
+        self.dfx = dfx
+        self.err = err
+
+    def print(self):
+        print(self.i,self.x,self.fx,self.dfx,self.err)
+
+
+class NewtonRaphson:
+
+    def __init__(self, x0):
+        self.x0 = x0
+
+    @staticmethod
+    def f(x):
+        return pow(x-4,2)*(x+2)
+        return pow(x, 3) - 0.165 * pow(x, 2) + 3.993 * pow(10, -4)
+
+    @staticmethod
+    def df(x):
+        return 2*(x-4)*(x+2)+pow(x-4, 2)
+        return 3*pow(x, 2) - 0.165 * 2 * x
+
+    @staticmethod
+    def error(x_old, x_new):
+        if x_old is None or x_new is None:
+            return None
+        return math.fabs((x_new-x_old)/x_new)
+
+    def run(self, iterations=None, tolerance=None):
+        status_s = []
+        x_im1 = None
+        x_i = self.x0
+        if iterations is not None:
+            for i in range(iterations):
+                status_s.append(Status(i+1,
+                                       x_i,
+                                       NewtonRaphson.f(x_i),
+                                       NewtonRaphson.df(x_i),
+                                       NewtonRaphson.error(x_im1, x_i)))
+                x_im1 = x_i
+                x_i -= NewtonRaphson.f(x_i) / NewtonRaphson.df(x_i)
+            return status_s
+        elif tolerance is not None:
+            i = 0
+            tolerance = pow(10, -tolerance)
+            temp_tolerance = None
+            while temp_tolerance is None or temp_tolerance > tolerance:
+                status_s.append(Status(i+1,
+                                       x_i,
+                                       NewtonRaphson.f(x_i),
+                                       NewtonRaphson.df(x_i),
+                                       NewtonRaphson.error(x_im1, x_i)))
+
+                temp_tolerance = NewtonRaphson.error(x_im1, x_i)
+                x_im1 = x_i
+                x_i -= NewtonRaphson.f(x_i) / NewtonRaphson.df(x_i)
+                i += 1
+            return status_s
+
+
+nr = NewtonRaphson(-2.3).run(tolerance=4)
+
+for n in nr:
+    n.print()
+
+nr = NewtonRaphson(0.05).run(iterations=5)
+
+for n in nr:
+    n.print()
+
+
+
--- SecantMethod.py
+++ SecantMethod.py
@@ -1,16 +1,84 @@
-def funct(x):
-    return pow(x, 3) - 0.165 * pow(x, 2) + 3.993 * pow(10, -4)
+import math
+import matplotlib.pyplot as plt
 
-def new(x_i,x_i_1):
-    return x_i-funct(x_i)*(x_i-x_i_1)/(funct(x_i)-funct(x_i_1))
 
-x_i=0.02
-x_i_1=0.05
+class Status:
+    def __init__(self,i, x, x_i_m_1, fx, dfx, err):
+        self.i = i
+        self.x = x
+        self.x_i_m_1 = x_i_m_1
+        self.fx = fx
+        self.dfx = dfx
+        self.err = err
 
-for i in range(6):
-    n=new(x_i,x_i_1)
-    print(n)
-    print(funct(n))
-    print()
-    x_i=x_i_1
-    x_i_1=n
+    def print(self):
+        print(self.i,self.x,self.x_i_m_1,self.fx,self.dfx,self.err)
+
+
+class SecantMethod:
+
+    def __init__(self, x0, x1):
+        self.x0 = x0
+        self.x1 = x1
+
+    @staticmethod
+    def f(x):
+        return pow(x-4,2)*(x+2)
+        return pow(x, 3) - 0.165 * pow(x, 2) + 3.993 * pow(10, -4)
+
+    @staticmethod
+    def df(x_i_m_1, x_i):
+        return (SecantMethod.f(x_i)-SecantMethod.f(x_i_m_1))/(x_i-x_i_m_1)
+
+    @staticmethod
+    def error(x_old, x_new):
+        if x_old is None or x_new is None:
+            return None
+        return math.fabs((x_new-x_old)/x_new)
+
+    def run(self, iterations=None, tolerance=None):
+        status_s = []
+        x_im1 = self.x0
+        x_i = self.x1
+        if iterations is not None:
+            for i in range(iterations):
+                status = Status(i + 1,
+                                       x_i,
+                               x_im1,
+                                       SecantMethod.f(x_i),
+                                       SecantMethod.df(x_im1, x_i),
+                                       SecantMethod.error(x_im1, x_i))
+                status_s.append(status)
+                temp = SecantMethod.f(x_i) / SecantMethod.df(x_im1,x_i)
+                x_im1 = x_i
+                x_i -= temp
+            return status_s
+        elif tolerance is not None:
+            i = 0
+            tolerance = pow(10, -tolerance)
+            temp_tolerance = None
+            while temp_tolerance is None or temp_tolerance > tolerance:
+                status = Status(i + 1,
+                                x_i,
+                                x_im1,
+                                SecantMethod.f(x_i),
+                                SecantMethod.df(x_im1, x_i),
+                                SecantMethod.error(x_im1, x_i))
+                status_s.append(status)
+                temp_tolerance = SecantMethod.error(x_im1, x_i)
+
+                temp = SecantMethod.f(x_i) / SecantMethod.df(x_im1, x_i)
+                x_im1 = x_i
+                x_i -= temp
+                i += 1
+            return status_s
+
+
+nr = SecantMethod(2.5,-1).run(tolerance=4)
+
+
+for n in nr:
+    n.print()
+
+
+
